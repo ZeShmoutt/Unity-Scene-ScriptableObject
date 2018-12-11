09e79ca2c0f8ecec3735d930cbf42fe878aa04f3
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2018-12-11 18:05:34 +0100

    Initial commit

---
+++ SceneData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ZeShmouttsAssets.Effects
+{
+	[CreateAssetMenu(fileName = "New Scene Data", menuName = "ZeShmoutt's Assets/Data Containers/Scene")]
+	public class SceneData : ScriptableObject
+	{
+#if UNITY_EDITOR
+		public UnityEditor.SceneAsset scene;
+#endif
+		public string sceneName;
+		public int sceneInt;
+	} 
+}
---
+++ SceneData_Editor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace ZeShmouttsAssets.Effects.EditorScripts
+{
+	[CustomEditor(typeof(SceneData))]
+	[CanEditMultipleObjects]
+	public class SceneData_Editor : Editor
+	{
+		#region Variables
+
+		private SceneData script;
+
+		#endregion
+
+		#region GUI
+
+		public override void OnInspectorGUI()
+		{
+			script = (SceneData)target;
+
+			UIScene();
+
+			EditorUtility.SetDirty(script);
+		}
+
+		#endregion
+
+		#region Parts
+
+		void UIScene()
+		{
+			script.scene = EditorGUILayout.ObjectField("UI Scene", script.scene, typeof(SceneAsset), false) as SceneAsset;
+			script.sceneName = (script.scene != null) ? script.scene.name : "";
+			script.sceneInt = (script.scene != null) ? SceneManager.GetSceneByName(script.sceneName).buildIndex : -2;
+		}
+
+		#endregion
+	}
+}
