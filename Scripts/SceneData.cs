3cc2316a27dd6392ad4ae4e278fec2268d864f5a
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2018-12-11 18:22:10 +0100

    Update SceneData.cs

--- Scripts/SceneData.cs
+++ Scripts/SceneData.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace ZeShmouttsAssets.Effects
 {
 	[CreateAssetMenu(fileName = "New Scene Data", menuName = "ZeShmoutt's Assets/Data Containers/Scene")]
 	public class SceneData : ScriptableObject
 	{
-#if UNITY_EDITOR
+		#if UNITY_EDITOR
 		public UnityEditor.SceneAsset scene;
-#endif
-		public string sceneName;
-		public int sceneInt;
+		#endif
+		
+		public string SceneName { get { return sceneName; } }
+		public int SceneIndex { get { return sceneIndex; } }
+
+		[SerializeField] private string sceneName;
+		[SerializeField] private int sceneIndex;
 	} 
 }
