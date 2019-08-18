c44ad4be96027503fe2051b7fa6fec44bdc62b95
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2019-08-18 23:32:16 +0200

    Commented, improved and securized

---
+++ ZeShmouttsAssets/SceneAsScriptableObject/Scripts/Editor/SceneData_Editor.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ZeShmouttsAssets.DataContainers.EditorScripts
+{
+	[CustomEditor(typeof(SceneData))]
+	[CanEditMultipleObjects]
+	public class SceneData_Editor : Editor
+	{
+		#region Variables
+
+		private SceneData script;
+
+		SerializedProperty sceneObject;
+		SerializedProperty sceneName;
+		SerializedProperty sceneIndex;
+		SerializedProperty scenePath;
+
+		#endregion
+
+		#region Unity Editor GUI
+
+		private void OnEnable()
+		{
+			script = (SceneData)target;
+
+			sceneObject = serializedObject.FindProperty("scene");
+			sceneName = serializedObject.FindProperty("sceneName");
+			sceneIndex = serializedObject.FindProperty("sceneIndex");
+			scenePath = serializedObject.FindProperty("scenePath");
+
+			EditorBuildSettings.sceneListChanged += HandleExternalSceneListChange;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			DrawScriptField();
+
+			EditorGUILayout.Space();
+
+			UIScene();
+
+			if (GUI.changed)
+			{
+				Undo.RecordObject(target, "Field change");
+				serializedObject.ApplyModifiedProperties();
+			}
+		}
+
+		private void OnDisable()
+		{
+			EditorBuildSettings.sceneListChanged -= HandleExternalSceneListChange;
+		}
+
+		private void HandleExternalSceneListChange()
+		{
+			script.OnBeforeSerialize();
+			Repaint();
+		}
+
+		#endregion
+
+		#region Parts
+
+		/// <summary>
+		/// Draws a script field similar to the regular inspector, allowing quick access to the script itself.
+		/// </summary>
+		protected void DrawScriptField()
+		{
+			bool wasEnabled = GUI.enabled;
+
+			GUI.enabled = false;
+			SerializedProperty prop = serializedObject.FindProperty("m_Script");
+			EditorGUILayout.PropertyField(prop, true);
+			GUI.enabled = wasEnabled;
+		}
+
+		/// <summary>
+		/// Parent for drawing editor settings.
+		/// </summary>
+		void UIScene()
+		{
+			bool sceneNotNull = sceneObject.objectReferenceValue != null;
+
+			EditorGUILayout.BeginVertical(GUI.skin.box);
+
+			EditorGUILayout.PropertyField(sceneObject, false);
+
+			script.OnBeforeSerialize();
+
+			EditorGUI.indentLevel++;
+
+			bool wasEnabled = GUI.enabled;
+
+			GUI.enabled = false;
+
+			string pathValue = scenePath.stringValue;
+			string nameValue = sceneName.stringValue;
+			int indexValue = sceneIndex.intValue;
+
+			EditorGUILayout.LabelField("Path", sceneNotNull ? pathValue : "N/A");
+
+			EditorGUILayout.LabelField("Name", sceneNotNull ? nameValue : "N/A");
+
+			EditorGUILayout.LabelField("Build Index", sceneNotNull && indexValue >= -1 ? indexValue.ToString() : "N/A");
+
+			GUI.enabled = wasEnabled;
+
+			EditorGUI.indentLevel--;
+
+			EditorGUILayout.EndVertical();
+
+			if (sceneNotNull)
+			{
+				if (indexValue < 0)
+				{
+					NotInBuildSettings(pathValue);
+				}
+				else if (indexValue >= 0 && EditorBuildSettings.scenes.Length >= indexValue + 1 && !EditorBuildSettings.scenes[indexValue].enabled)
+				{
+					DisabledInBuildSettings(sceneIndex.intValue);
+				}
+			}
+			else
+			{
+				EditorGUILayout.HelpBox("No scene selected.", MessageType.Error);
+			}
+		}
+
+		/// <summary>
+		/// Warning info label + button drawn when the selected scene isn't in the build settings.
+		/// </summary>
+		void NotInBuildSettings(string path)
+		{
+			EditorGUILayout.BeginHorizontal();
+
+			EditorGUILayout.HelpBox("The selected scene is not in the build settings.", MessageType.Warning);
+			if (GUILayout.Button("Add to build", GUILayout.Width(100), GUILayout.Height(40)))
+			{
+				EditorBuildSettingsScene sceneToAdd = new EditorBuildSettingsScene(path, true);
+				List<EditorBuildSettingsScene> scenesInBuild = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
+				scenesInBuild.Add(sceneToAdd);
+				EditorBuildSettings.scenes = scenesInBuild.ToArray();
+
+				Debug.LogFormat(sceneObject.objectReferenceValue, "Scene '{0}' has been added to build settings at index {1}.", path, scenesInBuild.Count - 1);
+			}
+
+			EditorGUILayout.EndHorizontal();
+		}
+
+		/// <summary>
+		/// Warning info label + button drawn when the selected scene is disabled in the build settings.
+		/// </summary>
+		void DisabledInBuildSettings(int index)
+		{
+			EditorGUILayout.BeginHorizontal();
+
+			EditorGUILayout.HelpBox("The selected scene is disabled in the build settings.", MessageType.Warning);
+			if (GUILayout.Button("Enable in build", GUILayout.Width(100), GUILayout.Height(40)))
+			{
+				EditorBuildSettingsScene[] buildSettingsScenes = EditorBuildSettings.scenes;
+				buildSettingsScenes[index].enabled = true;
+				EditorBuildSettings.scenes = buildSettingsScenes;
+
+				Debug.LogFormat(sceneObject.objectReferenceValue, "Scene #{0} has been enabled.", index);
+			}
+
+			EditorGUILayout.EndHorizontal();
+		}
+
+		#endregion
+	}
+}
---
+++ ZeShmouttsAssets/SceneAsScriptableObject/Scripts/SceneData.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ZeShmouttsAssets.DataContainers
+{
+	[CreateAssetMenu(fileName = "New Scene Data", menuName = "ZeShmoutt's Assets/Data Containers/Scene")]
+	public class SceneData : ScriptableObject, ISerializationCallbackReceiver
+	{
+		#region Variables
+
+#if UNITY_EDITOR
+#pragma warning disable 0649
+		/// <summary>
+		/// Editor-only scene object. Do not use in a runtime script !
+		/// </summary>
+		[SerializeField] private UnityEditor.SceneAsset scene;
+#pragma warning restore 0649
+#endif
+
+		/// <summary>
+		/// The scene's name.
+		/// </summary>
+		public string SceneName { get { return sceneName; } }
+
+		/// <summary>
+		/// The scene's build index.
+		/// </summary>
+		public int SceneIndex { get { return sceneIndex; } }
+
+		/// <summary>
+		/// The scene's path.
+		/// </summary>
+		public string ScenePath { get { return scenePath; } }
+
+		/// <summary>
+		/// Internal scene name.
+		/// </summary>
+		[SerializeField] private string sceneName = null;
+
+		/// <summary>
+		/// Internal scene index.
+		/// </summary>
+		[SerializeField] private int sceneIndex = -2;
+
+		/// <summary>
+		/// Internal scene path.
+		/// </summary>
+		[SerializeField] private string scenePath = null;
+
+		#endregion
+
+		#region Serialization Callback
+
+		public void OnAfterDeserialize()
+		{
+
+		}
+
+		public void OnBeforeSerialize()
+		{
+#if UNITY_EDITOR
+
+			string nameValue = (scene != null) ? scene.name : null;
+			int indexValue = -2;
+			string pathValue = (scene != null) ? UnityEditor.AssetDatabase.GetAssetPath(scene) : null;
+
+			UnityEditor.EditorBuildSettingsScene[] buildSettingsScenes = UnityEditor.EditorBuildSettings.scenes;
+			if (buildSettingsScenes.Length > 0)
+			{
+				for (int i = 0; i < buildSettingsScenes.Length; i++)
+				{
+					if (UnityEditor.EditorBuildSettings.scenes[i].path == pathValue)
+					{
+						indexValue = i;
+						break;
+					}
+				}
+			}
+
+			sceneName = nameValue;
+			sceneIndex = indexValue;
+			scenePath = pathValue;
+
+#endif
+		}
+
+		#endregion
+
+		#region Load From Default
+
+		/// <summary>
+		/// Shortcut to load the scene asynchronously in the background.
+		/// </summary>
+		/// <param name="mode">If LoadSceneMode.Single then all current Scenes will be unloaded before loading.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsync(LoadSceneMode mode)
+		{
+			if (sceneIndex >= 0)
+			{
+				return LoadAsyncFromBuildIndex(mode);
+			}
+			else
+			{
+				return LoadAsyncFromPath(mode);
+			}
+		}
+
+		/// <summary>
+		/// Shortcut to load the Scene asynchronously in the background.
+		/// </summary>
+		/// <param name="parameters">Struct that collects the various parameters into a single place except for the name and index.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsync(LoadSceneParameters parameters)
+		{
+			if (sceneIndex >= 0)
+			{
+				return LoadAsyncFromBuildIndex(parameters);
+			}
+			else
+			{
+				return LoadAsyncFromPath(parameters);
+			}
+		}
+
+		#endregion
+
+		#region Load From Name
+
+		/// <summary>
+		/// Shortcut to load the scene asynchronously in the background, using the scene's name.
+		/// </summary>
+		/// <param name="mode">If LoadSceneMode.Single then all current Scenes will be unloaded before loading.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromName(LoadSceneMode mode)
+		{
+			return SceneManager.LoadSceneAsync(sceneName, mode);
+		}
+
+		/// <summary>
+		/// Shortcut to load the Scene asynchronously in the background, using the scene's name.
+		/// </summary>
+		/// <param name="parameters">Struct that collects the various parameters into a single place except for the name and index.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromName(LoadSceneParameters parameters)
+		{
+			return SceneManager.LoadSceneAsync(sceneName, parameters);
+		}
+
+		#endregion
+
+		#region Load From Path
+
+		/// <summary>
+		/// Shortcut to load the scene asynchronously in the background, using the scene's path.
+		/// </summary>
+		/// <param name="mode">If LoadSceneMode.Single then all current Scenes will be unloaded before loading.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromPath(LoadSceneMode mode)
+		{
+			return SceneManager.LoadSceneAsync(scenePath, mode);
+		}
+
+		/// <summary>
+		/// Shortcut to load the Scene asynchronously in the background, using the scene's path.
+		/// </summary>
+		/// <param name="parameters">Struct that collects the various parameters into a single place except for the name and index.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromPath(LoadSceneParameters parameters)
+		{
+			return SceneManager.LoadSceneAsync(scenePath, parameters);
+		}
+
+		#endregion
+
+		#region Load From Build Index
+
+		/// <summary>
+		/// Shortcut to load the scene asynchronously in the background, using the scene's build index.
+		/// </summary>
+		/// <param name="mode">If LoadSceneMode.Single then all current Scenes will be unloaded before loading.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromBuildIndex(LoadSceneMode mode)
+		{
+			return SceneManager.LoadSceneAsync(sceneIndex, mode);
+		}
+
+		/// <summary>
+		/// Shortcut to load the Scene asynchronously in the background, using the scene's build index.
+		/// </summary>
+		/// <param name="parameters">Struct that collects the various parameters into a single place except for the name and index.</param>
+		/// <returns>Use the AsyncOperation to determine if the operation has completed.</returns>
+		public AsyncOperation LoadAsyncFromBuildIndex(LoadSceneParameters parameters)
+		{
+			return SceneManager.LoadSceneAsync(sceneIndex, parameters);
+		}
+
+		#endregion
+	}
+}
